49db67e0d75da2e3220f9452cc8ebc5ed2e26ba9
Carrot <11172391+CRThu@users.noreply.github.com>
2021-12-06 00:31:22 +0800

    加入图表datazoom与dataset

--- CarrotQuant.Net/Model/CandleData.cs
+++ CarrotQuant.Net/Model/CandleData.cs
@@ -12,21 +12,33 @@ namespace CarrotQuant.Net.Model
         public string StockCode { get; set; }
         public List<string> DateTime { get; set; } = new();
         public List<double[]> OHLC { get; set; } = new();
-        public List<double?> MA5 { get; set; } = new();
-        public List<double?> MA20 { get; set; } = new();
+
+        // 技术指标dataset
+        public string[] IndicatorsName { get; set; } = Array.Empty<string>();
+        public List<dynamic[]> IndicatorsData { get; set; } = new();
+
+        public CandleData()
+        {
+            IndicatorsName = new[] { "DateTime", "MA5", "MA20" };
+        }
 
         public void Add(string dateTime, double open, double high, double low, double close)
         {
             DateTime.Add(dateTime);
             OHLC.Add(new[] { open, close, low, high });
+
+            // Add Indicators
+            double? MA5;
+            double? MA20;
             if (OHLC.Count < 5)
-                MA5.Add(null);
+                MA5 = null;
             else
-                MA5.Add(OHLC.ToArray()[^5..].Select(k => k[1]).Sum() / 5);
+                MA5 = OHLC.ToArray()[^5..].Select(k => k[1]).Sum() / 5;
             if (OHLC.Count < 20)
-                MA20.Add(null);
+                MA20 = null;
             else
-                MA20.Add(OHLC.ToArray()[^20..].Select(k => k[1]).Sum() / 20);
+                MA20 = OHLC.ToArray()[^20..].Select(k => k[1]).Sum() / 20;
+            IndicatorsData.Add(new dynamic[] { dateTime, MA5, MA20 });
         }
     }
 }
