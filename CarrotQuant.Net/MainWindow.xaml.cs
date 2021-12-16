d9cd9ec2f14721e899b85feccd81d7d5bd992b2e
Carrot <11172391+CRThu@users.noreply.github.com>
2021-12-16 23:44:48 +0800

    图表5万点大数据量优化
    Former-commit-id: 38d77ba288f79e28d26d7df92f73bdde5ffec2a7

--- CarrotQuant.Net/MainWindow.xaml.cs
+++ CarrotQuant.Net/MainWindow.xaml.cs
@@ -36,14 +36,14 @@ namespace CarrotQuant.Net
         }
 
         DateTime dateTime = DateTime.Parse("2020-01-01");
-        EChartsData cd = new(new[] { 5, 10 }, new[] { 0, 0 }, 1);
+        EChartsData cd = new(new[] { 5, 10 }, new[] { 0, 0 }, 3);
         int trend = 100;
         Random randomNum = new Random(Guid.NewGuid().GetHashCode());
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             cd.StockName = "证券名称";
             cd.StockCode = "SH888888";
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 dateTime = dateTime.AddDays(1);
                 cd.AddTick(dateTime.ToString("yyyy-MM-dd"),
