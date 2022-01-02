b3ed4041096dc51d60a1f183253c8f6e7e86b561
Carrot <11172391+CRThu@users.noreply.github.com>
2022-01-03 00:45:50 +0800

    ECharts数据类封装
    Former-commit-id: 0d7373bf5cabf1972563d04242a86f7805243dad

--- CarrotQuant.Net/Model/EChartsData/EChartsData.cs
+++ CarrotQuant.Net/Model/EChartsData/EChartsData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CarrotQuant.Net.Model.EChartsData
@@ -10,19 +11,56 @@ namespace CarrotQuant.Net.Model.EChartsData
     public class EChartsData
     {
         // 图表标题(股票名称+股票代码)
-        public string StockName { get; set; } = "";
-        public string StockCode { get; set; } = "";
+        public string StockName { get; set; }
+        public string StockCode { get; set; }
 
         // 主副图数量:主图,[+副图1,][+副图2]
-        public int GridsCount { get; set; } = 1;
+        public int GridsCount { get; set; }
 
         // 图表集合
-        public List<EChartsSeries> Series { get; set; } = new();
+        public List<EChartsSeries> Series { get; set; }
 
         // 数据源集合
-        public List<string> Dimension { get; set; } = new();
-        public List<dynamic[]> Data { get; set; } = new();
+        public List<string> Dimension { get; set; }
+        public List<dynamic[]> Data { get; set; }
 
+        // 构造函数
+        public EChartsData()
+        {
+            StockName = string.Empty;
+            StockCode = string.Empty;
+            GridsCount = 1;
+            Series = new();
+            Dimension = new();
+            Data = new();
+        }
+
+        public EChartsData(string stockName, string stockCode, int gridsCount = 1) : this()
+        {
+            StockName = stockName;
+            StockCode = stockCode;
+            GridsCount = gridsCount;
+        }
+
+        // 添加Series
+        public void AddSeries(string name, EChartSeriesType type, int gridIndex, string dataXColumnName, string dataYColumnName)
+        {
+            Series.Add(new EChartsSeries(name, type, gridIndex, dataXColumnName, dataYColumnName));
+        }
+
+        public void AddSeries(string name, EChartSeriesType type, int gridIndex, string dataXColumnName, string[] dataYColumnsName)
+        {
+            Series.Add(new EChartsSeries(name, type, gridIndex, dataXColumnName, dataYColumnsName));
+        }
+
+        // 添加Data
+        public void AddData(string dimension, dynamic[] data)
+        {
+            Dimension.Add(dimension);
+            Data.Add(data);
+        }
+
+        // 序列化Json
         public string ToJson()
         {
             var jsons = JsonSerializer.Serialize(this);
--- CarrotQuant.Net/Model/EChartsData/EChartsSeries.cs
+++ CarrotQuant.Net/Model/EChartsData/EChartsSeries.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CarrotQuant.Net.Model.EChartsData
@@ -9,11 +10,12 @@ namespace CarrotQuant.Net.Model.EChartsData
     public class EChartsSeries
     {
         // 图表名称
-        public string Name { get; set; } = "";
+        public string Name { get; set; }
 
         // 图表类型
-        private string type = "";
-        public string Type
+        private EChartSeriesType type;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public EChartSeriesType Type
         {
             get
             {
@@ -29,29 +31,58 @@ namespace CarrotQuant.Net.Model.EChartsData
         // 图表显示在主图/副图
         public int GridIndex { get; set; }
 
-        // 其他配置
-        public Dictionary<string, dynamic> Options { get; set; } = new();
+        // 其他配置(修改Type时Setter自动更新默认配置)
+        public Dictionary<string, dynamic> Options { get; set; }
 
         // 坐标数据对应数据源维度
-        public List<string> DataXColumnsName { get; set; } = new();
-        public List<string> DataYColumnsName { get; set; } = new();
+        public List<string> DataXColumnsName { get; set; }
+        public List<string> DataYColumnsName { get; set; }
+
+        // 构造函数
+        public EChartsSeries()
+        {
+            Name = string.Empty;
+            Type = EChartSeriesType.line;
+            GridIndex = 0;
+            // Options = new();
+            DataXColumnsName = new();
+            DataYColumnsName = new();
+        }
+
+        public EChartsSeries(string name, EChartSeriesType type, int gridIndex, string dataXColumnName, string dataYColumnName) : this()
+        {
+            Name = name;
+            Type = type;
+            GridIndex = gridIndex;
+            DataXColumnsName = new() { dataXColumnName };
+            DataYColumnsName = new() { dataYColumnName };
+        }
+
+        public EChartsSeries(string name, EChartSeriesType type, int gridIndex, string dataXColumnName, string[] dataYColumnsName) : this()
+        {
+            Name = name;
+            Type = type;
+            GridIndex = gridIndex;
+            DataXColumnsName = new() { dataXColumnName };
+            DataYColumnsName = new(dataYColumnsName);
+        }
 
         // 生成每种图表默认配置
-        public static Dictionary<string, dynamic> GenerateDefaultOptions(string type)
+        public static Dictionary<string, dynamic> GenerateDefaultOptions(EChartSeriesType type)
         {
             Dictionary<string, dynamic> options = new();
             switch (type)
             {
-                case "candlestick":
+                case EChartSeriesType.candlestick:
                     options.Add("large", true);
                     break;
-                case "line":
+                case EChartSeriesType.line:
                     options.Add("large", true);
                     options.Add("linestyle", new Dictionary<string, dynamic> { { "width", 1 } });
                     options.Add("symbol", "none");
                     options.Add("sampling", "lttb");
                     break;
-                case "bar":
+                case EChartSeriesType.bar:
                     options.Add("large", true);
                     options.Add("sampling", "lttb");
                     break;
