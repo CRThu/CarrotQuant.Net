35e82c088b0798fdf8ab16035ecde64e895684b1
Carrot <11172391+CRThu@users.noreply.github.com>
2022-03-05 23:36:05 +0800

    回测框架DataFeed模块编程
    Former-commit-id: 52060217e02ce7dcd73b34ef569c0f49d9b4d11a

---
+++ CarrotBacktesting.NET/DataFeed/ShareData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.NET.DataFeed
+{
+    public class ShareData
+    {
+        /// <summary>
+        /// key为数据列名(例如OHLC), Value为对应数据
+        /// </summary>
+        public Dictionary<string, double[]> Data { get; set; }
+        public Dictionary<string, DateTime> Time { get; set; }
+
+        public ShareData(DataTable dataTable, string TimeColName, params string[] DataColName)
+        {
+            var ids = dataTable.AsEnumerable().Select(r => r.Field<int>("id")).ToList();
+            // TODO
+        }
+    }
+}
