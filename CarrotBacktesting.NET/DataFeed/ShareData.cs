d35f1a69d4ecb59491575d155fcce3fd490e24e9
Carrot <11172391+CRThu@users.noreply.github.com>
2022-03-16 02:04:59 +0800

    ShareData更新价格获取方法

--- CarrotBacktesting.NET/DataFeed/ShareData.cs
+++ CarrotBacktesting.NET/DataFeed/ShareData.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace CarrotBacktesting.NET.DataFeed
         public Dictionary<string, double[]> Data { get; set; }
 
         public string TimeDisplayName { get; set; }
-        public string[] Time { get; set; }
+        public DateTime[] Time { get; set; }
 
         public int Count => Time.Length;
         public double[] this[string key] => Data[key];
@@ -25,7 +26,7 @@ namespace CarrotBacktesting.NET.DataFeed
         public ShareData()
         {
             Data = new Dictionary<string, double[]>();
-            Time = Array.Empty<string>();
+            Time = Array.Empty<DateTime>();
         }
 
         public ShareData(DataTable dataTable, string timeColName, string[] dataColNames)
@@ -42,17 +43,73 @@ namespace CarrotBacktesting.NET.DataFeed
         /// <returns></returns>
         public static void DataTable2ShareData(ShareData shareData, DataTable dataTable, string timeColName, string[] dataColNames)
         {
-            shareData.Time = DataTableMisc.GetColumn<string>(dataTable, timeColName).ToArray();
+#if DEBUG
+            Debug.WriteLine($"ShareData.DataTable2ShareData(...) called.");
+            Stopwatch sw = new();
+            sw.Start();
+#endif
+            var timeArray = DataTableMisc.GetColumn<string>(dataTable, timeColName);
+            shareData.Time = timeArray.Select(t => DateTimeMisc.Parse(t)).ToArray();
+
             shareData.Data = new Dictionary<string, double[]>();
             foreach (var dataColName in dataColNames)
                 shareData.Data.Add(dataColName, DataTableMisc.GetColumn<string>(dataTable, dataColName)
                     .Select(s => Convert.ToDouble(s)).ToArray());
             shareData.TimeDisplayName = timeColName;
+#if DEBUG
+            sw.Stop();
+            Debug.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds} ms.");
+#endif
         }
 
         public void DataTable2ShareData(DataTable dataTable, string timeColName, string[] dataColNames)
         {
             DataTable2ShareData(this, dataTable, timeColName, dataColNames);
+        }
+
+        /// <summary>
+        /// 获取时间对应的索引号, 若没有匹配到精确日期,则向后匹配最近的日期, ShareData中存储的时间必须为正序排列.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public int GetTimeIndex(DateTime dateTime)
+        {
+            int index = Array.BinarySearch(Time, dateTime);
+            if (index >= 0)
+                return index;
+            else
+            {
+                return (~index == Time.Length) ? (~index - 1) : (~index);
+            }
+        }
+
+        public int GetTimeIndex(int year, int month, int day)
+        {
+            return GetTimeIndex(new DateTime(year, month, day));
+        }
+
+        public int GetTimeIndex(int year, int month, int day, int hour, int minute, int second)
+        {
+            return GetTimeIndex(new DateTime(year, month, day, hour, minute, second));
+        }
+
+        public double GetPrice(DateTime datetime, string key)
+        {
+            int index = GetTimeIndex(datetime);
+            return GetPrice(index, key);
+        }
+
+        public double GetPrice(int index, string key)
+        {
+            return Data[key][index];
+        }
+
+        public double[] GetData(DateTime start, DateTime end, string key)
+        {
+            int startIndex = GetTimeIndex(start);
+            int endIndex = GetTimeIndex(end);
+            //TODO
+            return null;
         }
     }
 }
