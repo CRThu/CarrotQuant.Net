54771ea5c1703e359797b0011f77e684f9483aa7
Carrot <11172391+CRThu@users.noreply.github.com>
2022-03-22 01:05:52 +0800

    新建单元测试项目

---
+++ CarrotBackTesting.Net.UnitTest/Indicator/TechnicalIndicatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarrotBacktesting.NET.Indicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.NET.Indicator.Tests
+{
+    [TestClass()]
+    public class TechnicalIndicatorTests
+    {
+        [TestMethod()]
+        public void ArrayMoveBackTest()
+        {
+            double[] vs = new double[] { 1, 2, 3, 4, 5, 6, 0, 0, 0, 0 };
+            double[] exp = new double[] { 0, 0, 0, 0, 1, 2, 3, 4, 5, 6 };
+            int cnt = 6;
+            int idx = 4;
+            TechnicalIndicator.ArrayMoveBack(vs, idx, cnt);
+            Assert.AreEqual(string.Join(',', vs), string.Join(',', exp));
+        }
+
+        [TestMethod()]
+        public void MATest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EMATest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MACDTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void KDJTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
