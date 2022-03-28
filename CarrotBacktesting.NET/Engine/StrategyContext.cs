aeb805a0b014e0429ce59cd9dfa065ba58b9ac9f
Carrot <11172391+CRThu@users.noreply.github.com>
2022-03-29 02:01:59 +0800

    策略与回测引擎架构开发

---
+++ CarrotBacktesting.NET/Engine/StrategyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.NET.Engine
+{
+    public class StrategyContext
+    {
+        public StrategyContext()
+        {
+
+        }
+    }
+}
---
+++ CarrotBacktesting.NET/Strategy/BasicStrategy.cs
@@ -0,0 +1,32 @@
+﻿using CarrotBacktesting.NET.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.NET.Strategy
+{
+    public class BasicStrategy : IStrategy
+    {
+        public void Start(StrategyContext strategyContext)
+        {
+            Console.WriteLine("BasicStrategy.Start()");
+        }
+
+        public void End(StrategyContext strategyContext)
+        {
+            Console.WriteLine("BasicStrategy.End()");
+        }
+
+        public void PreNext(StrategyContext strategyContext)
+        {
+            Console.WriteLine("BasicStrategy.PreNext()");
+        }
+
+        public void Next(StrategyContext strategyContext)
+        {
+            Console.WriteLine("BasicStrategy.Next()");
+        }
+    }
+}
