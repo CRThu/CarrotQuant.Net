6459ee38ebe8bc3243cd38a062030ca86f36e312
Carrot <11172391+CRThu@users.noreply.github.com>
2022-04-04 23:42:04 +0800

    头寸管理类更新
    Former-commit-id: f49dedc914283c49155e7ce858c23b1be346d92a

--- CarrotBacktesting.NET/Portfolio/GeneralPosition.cs
+++ CarrotBacktesting.NET/Portfolio/GeneralPosition.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarrotBacktesting.Net.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,11 +39,42 @@ namespace CarrotBacktesting.NET.Portfolio
             Size = direction == PositionDirectionEnum.Long ? size : -size;
         }
 
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="generalOrder"></param>
         public GeneralPosition(GeneralOrder generalOrder)
         {
             ExchangeName = generalOrder.ExchangeName;
             ShareName = generalOrder.ShareName;
             Size = generalOrder.Position == PositionDirectionEnum.Long ? generalOrder.Size : -generalOrder.Size;
+        }
+
+        /// <summary>
+        /// 判断两个头寸类是否为同一标的
+        /// </summary>
+        /// <param name="positionA"></param>
+        /// <param name="positionB"></param>
+        /// <returns></returns>
+        public static bool IsSameShare(GeneralPosition positionA, GeneralPosition positionB)
+        {
+            return positionA.ExchangeName == positionB.ExchangeName
+            && positionA.ShareName == positionB.ShareName;
+        }
+
+        /// <summary>
+        /// 判断两个头寸类是否为同一标的
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public bool IsSameShare(GeneralPosition position)
+        {
+            return IsSameShare(this, position);
+        }
+
+        public override string ToString()
+        {
+            return JsonConverter.Serialize(this);
         }
     }
 }
--- CarrotBacktesting.NET/Portfolio/PositionManager.cs
+++ CarrotBacktesting.NET/Portfolio/PositionManager.cs
@@ -10,15 +10,30 @@ namespace CarrotBacktesting.Net.Portfolio
     public class PositionManager
     {
         public const string RESERVED_SYMBOLS = "$|";
-        public const string DEFAULT_CASH_KEY = "$CASH$";
+        public const string DEFAULT_CASH_NAME = "$CASH$";
         public Dictionary<string, GeneralPosition> Positions { get; set; } = new();
 
         /// <summary>
-        /// 构造函数
+        /// 构造函数, 默认初始化为100000现金
         /// </summary>
         public PositionManager()
         {
-            Positions.Add(DEFAULT_CASH_KEY, DEFAULT_CASH_VALUE);
+            SetCash(100000);
+        }
+
+        /// <summary>
+        /// 设置现金数量(存放在Position字典, Key:"$CASH$|$CASH$", Value.Size=1000)
+        /// </summary>
+        /// <param name="cash"></param>
+        /// <param name="exchangeName"></param>
+        public void SetCash(double cash, string exchangeName = DEFAULT_CASH_NAME)
+        {
+            string key = GenerateCashKey(exchangeName);
+            var position = GenerateCashPosition(cash, exchangeName);
+            if (!Positions.ContainsKey(key))
+                Positions.Add(key, position);
+            else
+                Positions[key] = position;
         }
 
         /// <summary>
@@ -31,16 +46,45 @@ namespace CarrotBacktesting.Net.Portfolio
             string key = GeneratePositionKey(value);
             if (!Positions.ContainsKey(key))
                 Positions.Add(key, value);
+            else if (Positions[key].IsSameShare(value))
+                Positions[key].Size += value.Size;
+            else
+                throw new Exception($"PositionManager程序键缓存错误,Key={key},Value={value}.");
+
+            // 若头寸为0,则移除头寸类
+            if (Positions[key].Size == 0)
+                Positions.Remove(key);
         }
 
+        /// <summary>
+        /// 生成Position字典头寸存储键
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
         public static string GeneratePositionKey(GeneralPosition position)
         {
             return $"{position.ExchangeName}|{position.ShareName}";
         }
 
-        public static GeneralPosition GenerateCashPosition(double cash)
+        /// <summary>
+        /// 生成Position字典现金存储键
+        /// </summary>
+        /// <param name="exchangeName"></param>
+        /// <returns></returns>
+        public static string GenerateCashKey(string exchangeName = DEFAULT_CASH_NAME)
         {
-            return new(DEFAULT_CASH_KEY, DEFAULT_CASH_KEY, cash, PositionDirectionEnum.Long);
+            return $"{exchangeName}|{DEFAULT_CASH_NAME}";
+        }
+
+        /// <summary>
+        /// 生成Position字典现金存储值, 返回现金的头寸类
+        /// </summary>
+        /// <param name="cash"></param>
+        /// <param name="exchangeName"></param>
+        /// <returns></returns>
+        public static GeneralPosition GenerateCashPosition(double cash, string exchangeName = DEFAULT_CASH_NAME)
+        {
+            return new(exchangeName, DEFAULT_CASH_NAME, cash, PositionDirectionEnum.Long);
         }
     }
 }
