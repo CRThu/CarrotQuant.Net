9bc5117e15741cfecf41e4d47a352ae2b593cbc3
Carrot <11172391+CRThu@users.noreply.github.com>
2022-04-08 04:11:35 +0800

    删除远程大写文件夹

--- CarrotBacktesting.NET/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CarrotBacktesting.Net.Engine;
-using CarrotBacktesting.Net.Strategy;
-
-Console.WriteLine("Hello, World!");
-
-IEngine engine = new BackTestingEngine(new BasicStrategy());
-engine.Run();
