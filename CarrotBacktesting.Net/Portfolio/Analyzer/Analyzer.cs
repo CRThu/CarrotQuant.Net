6514017d377b675fdf7b96837d7758b0e75cdc02
Carrot <11172391+CRThu@users.noreply.github.com>
2022-04-23 04:46:40 +0800

    投资组合分析器开发

--- CarrotBacktesting.Net/Portfolio/Analyzer/Analyzer.cs
+++ CarrotBacktesting.Net/Portfolio/Analyzer/Analyzer.cs
@@ -1,4 +1,5 @@
 ﻿using CarrotBacktesting.Net.Shared;
+using MathNet.Numerics.Statistics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,6 @@ namespace CarrotBacktesting.Net.Portfolio.Analyzer
     /// </summary>
     public class Analyzer
     {
-        public AnalyzerFrame[] AnalyzerFrames;
-
         /// <summary>
         /// 构造函数
         /// </summary>
@@ -22,33 +21,112 @@ namespace CarrotBacktesting.Net.Portfolio.Analyzer
         }
 
         /// <summary>
-        /// 更新分析器
+        /// 获取回撤曲线
         /// </summary>
-        /// <param name="pnlLogger"></param>
-        public void Update(PnlLogger pnlLogger)
+        /// <param name="value">总资产</param>
+        /// <returns></returns>
+        public static double[] GetDrawdown(double[] value)
         {
-            AnalyzerFrames = new AnalyzerFrame[pnlLogger.Logs.Count];
+            double[] drawdown = new double[value.Length];
 
             // 存储历史最高总资产用于计算回撤
             double peakDrawdown = 0;
 
-            for (int i = 0; i < pnlLogger.Logs.Count; i++)
+            for (int i = 0; i < value.Length; i++)
             {
-                AnalyzerFrames[i].DateTime = pnlLogger.Logs[i].DateTime;
-                AnalyzerFrames[i].TotalValue = pnlLogger.Logs[i].TotalValue;
-                AnalyzerFrames[i].PositionRatio = pnlLogger.Logs[i].PositionRatio;
-                AnalyzerFrames[i].TotalPnl = pnlLogger.Logs[i].TotalPnl;
-
-                // 回撤计算
-                if (AnalyzerFrames[i].TotalValue > peakDrawdown)
-                    peakDrawdown = AnalyzerFrames[i].TotalValue;
-                AnalyzerFrames[i].Drawdown = 1 - AnalyzerFrames[i].TotalValue / peakDrawdown;
+                // 回撤计算(恒定本金法)
+                if (value[i] > peakDrawdown)
+                    peakDrawdown = value[i];
+                drawdown[i] = 1 - value[i] / peakDrawdown;
             }
+            return drawdown;
         }
 
-        public override string ToString()
+        /// <summary>
+        /// 获取最大回撤率
+        /// </summary>
+        /// <param name="value">总资产</param>
+        /// <returns></returns>
+        public static double GetMaxDrawdown(double[] value)
         {
-            return ClassFormatter.Formatter(AnalyzerFrames);
+            return GetDrawdown(value).Max();
+        }
+
+        /// <summary>
+        /// 计算每Tick收益
+        /// </summary>
+        /// <param name="pnl">每Tick总损益</param>
+        /// <returns></returns>
+        public static double[] GetReturn(double[] pnl)
+        {
+            double[] tickReturn = new double[pnl.Length];
+
+            if (pnl.Length > 0)
+            {
+                tickReturn[0] = pnl[0];
+                for (int i = 1; i < pnl.Length; i++)
+                    tickReturn[i] = pnl[i] - pnl[i - 1];
+            }
+            return tickReturn;
+        }
+
+        /// <summary>
+        /// 计算每Tick收益率
+        /// </summary>
+        /// <param name="pnl">每Tick总损益</param>
+        /// <param name="cost">成本</param>
+        /// <param name="normalize">归一化至单位收益</param>
+        /// <returns></returns>
+        public static double[] GetRateOfReturn(double[] pnl, double cost, double normalize = 1)
+        {
+            return GetReturn(pnl).Select(r => r / cost * normalize).ToArray();
+        }
+
+        /// <summary>
+        /// 获取夏普比率(Tick单位)
+        /// </summary>
+        /// <param name="pnl">每Tick总损益</param>
+        /// <returns></returns>
+        public static double GetSharpeRatio(double[] pnl)
+        {
+            double[] tickReturn = GetReturn(pnl);
+
+            // https://numerics.mathdotnet.com/DescriptiveStatistics.html
+            double mean = tickReturn.Mean();
+            double stddev = tickReturn.PopulationStandardDeviation();
+            return mean / stddev;
+        }
+
+        // TODO 重构以下函数
+
+        /// <summary>
+        /// 获取回撤曲线
+        /// </summary>
+        /// <param name="pnlLogger"></param>
+        /// <returns></returns>
+        public static double[] GetDrawdown(PnlLogger pnlLogger)
+        {
+            return GetDrawdown(pnlLogger.Logs.Select(l => l.TotalValue).ToArray());
+        }
+
+        /// <summary>
+        /// 获取最大回撤率
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static double GetMaxDrawdown(PnlLogger pnlLogger)
+        {
+            return GetDrawdown(pnlLogger).Max();
+        }
+
+        /// <summary>
+        /// 获取夏普比率
+        /// </summary>
+        /// <param name="pnlLogger"></param>
+        /// <returns></returns>
+        public static double GetSharpeRatio(PnlLogger pnlLogger)
+        {
+            return GetSharpeRatio(pnlLogger.Logs.Select(l => l.TotalPnl).ToArray());
         }
     }
 }
