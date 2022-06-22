af2fa056c2538108a09d56e7312536ed4436d260
Carrot <11172391+CRThu@users.noreply.github.com>
2022-06-23 00:58:08 +0800

    重构市场信息存储结构

---
+++ CarrotBacktesting.Net/DataFeed/NewMarketData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.DataFeed
+{
+    /// <summary>
+    /// 市场信息类
+    /// </summary>
+    public class NewMarketData
+    {
+        /// <summary>
+        /// key: 市场信息帧日期
+        /// value: 市场信息帧数据
+        /// </summary>
+        public Dictionary<DateTime, NewMarketFrame> MarketFrames { get; set; } = new();
+
+        // TODO
+    }
+}
---
+++ CarrotBacktesting.Net/DataFeed/NewMarketFrame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.DataFeed
+{
+    /// <summary>
+    /// 市场信息帧类
+    /// </summary>
+    public class NewMarketFrame
+    {
+        /// <summary>
+        /// 帧日期
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// 存放一帧市场信息
+        /// key: 股票代码
+        /// value: 股票帧
+        /// </summary>
+        public Dictionary<string, NewShareFrame> MarketFrame { get; set; } = new();
+    }
+}
