a50838576eba69981b0cb55d2325c082fa68316a
Carrot <11172391+CRThu@users.noreply.github.com>
2022-07-03 03:15:08 +0800

    重构市场信息存储结构与MarketFrame方法更新

--- CarrotBacktesting.Net/DataModel/MarketData.cs
+++ CarrotBacktesting.Net/DataModel/MarketData.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 namespace CarrotBacktesting.Net.DataModel
 {
     /// <summary>
-    /// 市场信息类
+    /// 市场信息帧集合类
     /// </summary>
     public class MarketData
     {
         /// <summary>
-        /// 市场帧集合
+        /// 包含多个时间与多支股票信息的市场帧集合
+        /// key: 时间
+        /// value: 市场帧
         /// </summary>
-        public List<MarketFrame> MarketFrames { get; set; } = new();
+        public Dictionary<DateTime, MarketFrame> MarketFrames { get; set; } = new();
     }
 }
--- CarrotBacktesting.Net/DataModel/MarketFrame.cs
+++ CarrotBacktesting.Net/DataModel/MarketFrame.cs
@@ -14,7 +14,7 @@ namespace CarrotBacktesting.Net.DataModel
         /// <summary>
         /// 帧日期
         /// </summary>
-        public DateTime DateTime { get; set; }
+        public DateTime? DateTime { get; set; }
 
         /// <summary>
         /// 存放一帧市场信息
@@ -22,5 +22,88 @@ namespace CarrotBacktesting.Net.DataModel
         /// value: 股票帧
         /// </summary>
         public Dictionary<string, ShareFrame> Frame { get; set; }
+
+        /// <summary>
+        /// 写入或读取股票在<see cref="DateTime"/>时间的信息帧
+        /// </summary>
+        /// <param name="stockCode">股票代码</param>
+        /// <returns>返回stockCode对应帧, 若stockCode不存在则返回null</returns>
+        /// <exception cref="ArgumentNullException">传入<see cref="ShareFrame"/>为空时抛出异常</exception>
+        /// <exception cref="InvalidOperationException">传入stockCode与<see cref="ShareFrame.StockCode"/>不一致时抛出异常</exception>
+        public ShareFrame? this[string stockCode]
+        {
+            get
+            {
+                return Get(stockCode);
+            }
+            set
+            {
+                if(value == null)
+                    throw new ArgumentNullException(nameof(value));
+                if (stockCode != value.Value.StockCode)
+                    throw new InvalidOperationException();
+
+                Set((ShareFrame)value);
+            }
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public MarketFrame()
+        {
+            DateTime = null;
+            Frame = new();
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="dateTime">市场帧日期/时间</param>
+        public MarketFrame(DateTime dateTime)
+        {
+            DateTime = dateTime;
+            Frame = new();
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="dateTime">市场帧日期/时间</param>
+        /// <param name="frame">市场帧数据字典</param>
+        public MarketFrame(DateTime dateTime, Dictionary<string, ShareFrame> frame)
+        {
+            DateTime = dateTime;
+            Frame = frame;
+        }
+
+        /// <summary>
+        /// 添加帧
+        /// </summary>
+        /// <param name="shareFrame">股票帧, 若已添加相同的股票代码则覆盖</param>
+        /// <exception cref="InvalidOperationException">新增数据帧与现有数据帧时间不符时抛出异常</exception>
+        public void Set(ShareFrame shareFrame)
+        {
+            if (DateTime is null)
+                DateTime = shareFrame.DateTime;
+            else if (shareFrame.DateTime != DateTime)
+                // 新增数据与现有数据时间不符时抛出异常
+                throw new InvalidOperationException();
+            // 添加数据
+            Frame[shareFrame.StockCode] = shareFrame;
+        }
+
+        /// <summary>
+        /// 获取帧
+        /// </summary>
+        /// <param name="stockCode">股票代码</param>
+        /// <returns>返回stockCode对应帧, 若stockCode不存在则返回null</returns>
+        public ShareFrame? Get(string stockCode)
+        {
+            if (Frame.TryGetValue(stockCode, out ShareFrame v))
+                return v;
+            else
+                return null;
+        }
     }
 }
