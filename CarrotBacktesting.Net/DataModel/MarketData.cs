62831db84d5ec395ecd900629b6c060258169989
Carrot <11172391+CRThu@users.noreply.github.com>
2022-07-07 03:11:16 +0800

    MarketFrame, MarketData方法更新, 新增MarketDataBuilder

--- CarrotBacktesting.Net/DataModel/MarketData.cs
+++ CarrotBacktesting.Net/DataModel/MarketData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarrotBacktesting.Net.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,20 @@ namespace CarrotBacktesting.Net.DataModel
         /// <summary>
         /// MarketFrames Keys时间集合缓存
         /// </summary>
-        private List<DateTime> DateTimesCache { get; set; }
+        private DateTime[] DateTimesCache { get; set; }
+
+        /// <summary>
+        /// 读取时间的市场信息, 若时间不存在则返回向前搜索最近的时间帧, 若向前搜索不存在帧则返回最早时间
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <returns>时间对应或相近的市场信息</returns>
+        public MarketFrame this[DateTime dateTime]
+        {
+            get
+            {
+                return Get(dateTime);
+            }
+        }
 
         /// <summary>
         /// 构造函数
@@ -35,18 +49,19 @@ namespace CarrotBacktesting.Net.DataModel
         public MarketData()
         {
             MarketFrames = new();
-            DateTimesCache = new();
+            DateTimesCache = Array.Empty<DateTime>();
         }
 
         /// <summary>
         /// 更新日期缓存
         /// </summary>
-        public void UpdateCache()
+        public void RefrehCache()
         {
             if (IsCacheDirty)
             {
-                DateTimesCache = MarketFrames.Keys.ToList();
-                DateTimesCache.Sort();
+                var dts = MarketFrames.Keys.ToList();
+                dts.Sort();
+                DateTimesCache = dts.ToArray();
                 IsCacheDirty = false;
             }
         }
@@ -55,38 +70,59 @@ namespace CarrotBacktesting.Net.DataModel
         /// 添加帧
         /// </summary>
         /// <param name="frame">市场信息帧, 若已添加相同时间的帧则覆盖</param>
-        public void Set(MarketFrame frame)
+        public void Add(MarketFrame frame)
         {
             IsCacheDirty = true;
 
-            if(frame.DateTime is null)
-                throw new InvalidOperationException();
-
             // 添加数据
-            MarketFrames[frame.DateTime.Value] = frame;
+            MarketFrames.Add(frame.DateTime, frame);
         }
 
         /// <summary>
-        /// 获取帧
+        /// 是否存在此时间的市场信息帧
         /// </summary>
-        /// <param name="dateTime">帧时间</param>
-        /// <param name="isExist">是否存在此时间帧</param>
-        /// <returns>返回帧, 若此时间帧不存在则返回向前搜索最近的时间帧, 若向前搜索不存在帧则返回null</returns>
-        public MarketFrame? Get(DateTime dateTime, out bool isExist)
+        /// <param name="dateTime">时间</param>
+        /// <returns>存在则返回true, 否则返回false</returns>
+        public bool Contains(DateTime dateTime)
         {
-            if (IsCacheDirty)
-                UpdateCache();
+            return MarketFrames.ContainsKey(dateTime);
+        }
 
-            if (MarketFrames.TryGetValue(dateTime, out MarketFrame? v))
-            {
-                isExist = true;
-                return v;
-            }
-            else
-            {
-                isExist = false;
-                return null;
-            }
+        /// <summary>
+        /// 获取此时间对应的市场信息帧
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <returns>此时间对应的市场信息帧</returns>
+        public MarketFrame Get(DateTime dateTime)
+        {
+            // 精确时间查找
+            return MarketFrames[dateTime];
+        }
+
+        /// <summary>
+        /// 获取此时间对应的市场信息帧
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <param name="marketFrame">此时间对应的市场信息帧</param>
+        /// <returns>若存在则返回true, 否则返回false</returns>
+        public bool TryGet(DateTime dateTime, out MarketFrame? marketFrame)
+        {
+            return MarketFrames.TryGetValue(dateTime, out marketFrame);
+        }
+
+        /// <summary>
+        /// 获取此时间对应或相近的市场信息帧
+        /// </summary>
+        /// <param name="dateTime">时间</param>
+        /// <returns>返回帧, 若此时间帧不存在则返回向前搜索最近的时间帧, 若向前搜索不存在帧则返回最早时间</returns>
+        public MarketFrame GetNearby(DateTime dateTime)
+        {
+            // 若时间缓存不是最新的则刷新
+            RefrehCache();
+
+            // 二分法查找最新时间, 若不存在则向前模糊时间查找
+            var dtnb = DateTimesCache.GetTimeNearby(dateTime);
+            return MarketFrames[dtnb];
         }
     }
 }
