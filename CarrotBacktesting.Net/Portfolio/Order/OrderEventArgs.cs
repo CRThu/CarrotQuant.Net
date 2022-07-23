6a57bf163d40d9e0ed6c37570a3b7fb512a26ba8
Carrot <11172391+CRThu@users.noreply.github.com>
2022-07-23 23:54:17 +0800

    委托单管理器重构

---
+++ CarrotBacktesting.Net/Portfolio/Order/OrderEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.Portfolio.Order
+{
+    /// <summary>
+    /// 委托单事件参数类
+    /// </summary>
+    public class OrderEventArgs
+    {
+        // int orderId, GeneralOrder order, OrderUpdatedEventOperation operation
+    }
+
+    public enum OrderUpdatedEventOperation
+    {
+        AddOrder,
+        RemoveOrder,
+        UpdateOrder
+    }
+
+}
