7aacdb78bcd916617469eb540577def0dfea6046
Carrot <11172391+CRThu@users.noreply.github.com>
2022-07-30 02:44:12 +0800

    头寸存储类重构

--- CarrotBacktesting.Net/Portfolio/Position/GeneralPosition.cs
+++ CarrotBacktesting.Net/Portfolio/Position/GeneralPosition.cs
@@ -9,46 +9,54 @@ using System.Threading.Tasks;
 namespace CarrotBacktesting.Net.Portfolio.Position
 {
     /// <summary>
-    /// 常规头寸存储类
+    /// 常规头寸
     /// </summary>
     public class GeneralPosition
     {
         /// <summary>
         /// 股票名称
         /// </summary>
-        public string ShareName { get; set; }
+        public string StockName { get; set; }
+
         /// <summary>
         /// 头寸大小
         /// </summary>
         public double Size { get; set; }
+
         /// <summary>
-        /// 头寸持仓成本
+        /// 头寸平均持仓成本
         /// </summary>
         public double Cost { get; set; }
+
         /// <summary>
-        /// 当前价格
-        /// </summary>
-        public double CurrentPrice { get; set; }
-        /// <summary>
-        /// 未实现损益
-        /// </summary>
-        public double UnRealizedPnl => (CurrentPrice - Cost) * Size;
-        /// <summary>
-        /// 已实现损益
-        /// </summary>
-        public double RealizedPnl { get; set; }
-        /// <summary>
-        /// 总损益
-        /// </summary>
-        public double TotalPnl => UnRealizedPnl + RealizedPnl;
-        /// <summary>
-        /// 成本价值
+        /// 头寸持仓成本总价值
         /// </summary>
         public double CostValue => Cost * Size;
-        /// <summary>
-        /// 当前价值
-        /// </summary>
-        public double CurrentValue => CurrentPrice * Size;
+
+        ///// <summary>
+        ///// 当前价格
+        ///// </summary>
+        //public double CurrentPrice { get; set; }
+
+        ///// <summary>
+        ///// 未实现损益
+        ///// </summary>
+        //public double UnRealizedPnl => (CurrentPrice - Cost) * Size;
+
+        ///// <summary>
+        ///// 已实现损益
+        ///// </summary>
+        //public double RealizedPnl { get; set; }
+
+        ///// <summary>
+        ///// 总损益
+        ///// </summary>
+        //public double TotalPnl => UnRealizedPnl + RealizedPnl;
+
+        ///// <summary>
+        ///// 当前价值
+        ///// </summary>
+        //public double CurrentValue => CurrentPrice * Size;
 
 
         /// <summary>
@@ -59,7 +67,7 @@ namespace CarrotBacktesting.Net.Portfolio.Position
         /// <param name="direction"></param>
         public GeneralPosition(string shareName, double size, double cost, OrderDirection direction = OrderDirection.Buy)
         {
-            ShareName = shareName;
+            StockName = shareName;
             Size = direction == OrderDirection.Buy ? size : -size;
             Cost = cost;
         }
@@ -70,30 +78,9 @@ namespace CarrotBacktesting.Net.Portfolio.Position
         /// <param name="generalOrder"></param>
         public GeneralPosition(GeneralOrder generalOrder, double price)
         {
-            ShareName = generalOrder.StockCode;
+            StockName = generalOrder.StockCode;
             Size = generalOrder.Direction == OrderDirection.Buy ? generalOrder.OrderSize : -generalOrder.OrderSize;
             Cost = price;
-        }
-
-        /// <summary>
-        /// 判断两个头寸类是否为同一标的
-        /// </summary>
-        /// <param name="positionA"></param>
-        /// <param name="positionB"></param>
-        /// <returns></returns>
-        public static bool IsSameShare(GeneralPosition positionA, GeneralPosition positionB)
-        {
-            return positionA.ShareName == positionB.ShareName;
-        }
-
-        /// <summary>
-        /// 判断两个头寸类是否为同一标的
-        /// </summary>
-        /// <param name="position"></param>
-        /// <returns></returns>
-        public bool IsSameShare(GeneralPosition position)
-        {
-            return IsSameShare(this, position);
         }
     }
 }
