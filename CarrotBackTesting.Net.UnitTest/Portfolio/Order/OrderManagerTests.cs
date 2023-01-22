39e9898f2e28698a382f431c54fb86105c57393a
Carrot <11172391+CRThu@users.noreply.github.com>
2023-01-22 22:46:10 +0800

    更新单元测试
    Former-commit-id: 74720adbd8f2b38a5b7dbe66bab1813d398a7048

---
+++ CarrotBackTesting.Net.UnitTest/Portfolio/Order/OrderManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarrotBacktesting.Net.Portfolio.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.Portfolio.Order.Tests
+{
+    [TestClass()]
+    public class OrderManagerTests
+    {
+        [TestMethod()]
+        public void GetOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CancelOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TradeOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OnTradeUpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
