ad0519f57025459b418439b5d009bff154711653
Carrot <11172391+CRThu@users.noreply.github.com>
2023-01-24 21:34:11 +0800

    更新单元测试
    Former-commit-id: 848b2d17cc5981b72da2de157031999f704d3520

--- CarrotBackTesting.Net.UnitTest/DataModel/MarketFrameTests.cs
+++ CarrotBackTesting.Net.UnitTest/DataModel/MarketFrameTests.cs
@@ -14,19 +14,72 @@ namespace CarrotBacktesting.Net.DataModel.Tests
         [TestMethod()]
         public void MarketFrameTest()
         {
+            DateTime t = new DateTime(2022, 01, 01, 11, 22, 33);
+            MarketFrame marketFrame1 = new(t);
 
+            ShareFrame? shareFrame1 = new(
+                stockCode: "SH.000001",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 10000, isTrading: true);
+            marketFrame1.Add(shareFrame1);
+            shareFrame1 = new(
+                stockCode: "SH.000002",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 20000, isTrading: true);
+            marketFrame1.Add(shareFrame1);
+            shareFrame1 = new(
+                stockCode: "SH.000003",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 30000, isTrading: true);
+            marketFrame1.Add(shareFrame1);
+            shareFrame1 = null;
+
+            Assert.AreEqual(3, marketFrame1.Count);
+            Assert.AreEqual(t, marketFrame1.DateTime);
+            Assert.AreEqual("SH.000001", marketFrame1["SH.000001"]!.StockCode);
+            Assert.AreEqual(10000, marketFrame1["SH.000001"]!.Volume);
+            Assert.AreEqual("SH.000002", marketFrame1["SH.000002"]!.StockCode);
+            Assert.AreEqual(20000, marketFrame1["SH.000002"]!.Volume);
+            Assert.AreEqual("SH.000003", marketFrame1["SH.000003"]!.StockCode);
+            Assert.AreEqual(30000, marketFrame1["SH.000003"]!.Volume);
         }
 
         [TestMethod()]
         public void MarketFrameTest1()
         {
+            DateTime t = new DateTime(2022, 01, 01, 11, 22, 33);
 
-        }
+            ShareFrame? shareFrame1 = new(
+                stockCode: "SH.000001",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 10000, isTrading: true);
+            ShareFrame? shareFrame2 = new(
+                stockCode: "SH.000002",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 20000, isTrading: true);
+            ShareFrame? shareFrame3 = new(
+                stockCode: "SH.000003",
+                dateTime: t,
+                openPrice: 1.5, highPrice: 3.5, lowPrice: 1.0, closePrice: 2.0,
+                volume: 30000, isTrading: true);
 
-        [TestMethod()]
-        public void AddTest()
-        {
-
+            Dictionary<string, ShareFrame> sfd = new()
+            {
+                { shareFrame1.StockCode, shareFrame1 },
+                { shareFrame2.StockCode, shareFrame2 },
+                { shareFrame3.StockCode, shareFrame3 },
+            };
+            MarketFrame marketFrame1 = new(t, sfd);
+            Assert.AreEqual(3, marketFrame1.Count);
+            Assert.AreEqual(t, marketFrame1.DateTime);
+            Assert.AreEqual(10000, marketFrame1["SH.000001"]!.Volume);
+            Assert.AreEqual(20000, marketFrame1["SH.000002"]!.Volume);
+            Assert.AreEqual(30000, marketFrame1["SH.000003"]!.Volume);
         }
     }
 }
