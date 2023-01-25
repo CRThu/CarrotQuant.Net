08f682092f4bc04f51e8ede79098abcd4bac7150
Carrot <11172391+CRThu@users.noreply.github.com>
2023-01-26 02:06:52 +0800

    更新Csv导入数据源与单元测试
    Former-commit-id: 53a7c6d60d6f47f1ebedfca99cfcf70ad50e94c5

---
+++ CarrotBackTesting.Net.UnitTest/Storage/CsvDataProviderTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarrotBacktesting.Net.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.Storage.Tests
+{
+    [TestClass()]
+    public class CsvDataProviderTests
+    {
+        [TestMethod()]
+        public void CsvDataProviderTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetShareDataTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetShareDataTest1()
+        {
+
+        }
+    }
+}
---
+++ CarrotBacktesting.Net/Storage/CsvDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using CarrotBacktesting.Net.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.Storage
+{
+    public class CsvDataProvider : IDataProvider
+    {
+
+        public CsvDataProvider()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShareFrame> GetShareData(string stockCode, string[] fields, DateTime? startTime = null, DateTime? endTime = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ShareFrame> GetShareData(string[] stockCode, string[] fields, DateTime? startTime = null, DateTime? endTime = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
