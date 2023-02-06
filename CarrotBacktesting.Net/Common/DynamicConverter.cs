4108cc23ef2ccc16a17e0c45d14017e6679b7b84
Carrot <11172391+CRThu@users.noreply.github.com>
2023-02-06 19:12:38 +0800

    DynamicConverter boolean类型支持
    Former-commit-id: 47eb4e0c18979a42c3e9aa2ac63e3e5c99c21ce3

--- CarrotBacktesting.Net/Common/DynamicConverter.cs
+++ CarrotBacktesting.Net/Common/DynamicConverter.cs
@@ -6,8 +6,58 @@ using System.Threading.Tasks;
 
 namespace CarrotBacktesting.Net.Common
 {
+    /// <summary>
+    /// 通用类型转换器
+    /// </summary>
     public static class DynamicConverter
     {
+        /// <summary>
+        /// Boolean类型True值字符串附加转换字段
+        /// </summary>
+        private static readonly HashSet<string> BooleanExTrueStrings = new() { "yes", "1", "是" };
+        /// <summary>
+        /// Boolean类型False值字符串附加转换字段
+        /// </summary>
+        private static readonly HashSet<string> BooleanExFalseStrings = new() { "no", "0", "否" };
+
+        /// <summary>
+        /// Boolean类型添加True值字符串方法
+        /// </summary>
+        /// <param name="trueStrings">字符串集合</param>
+        public static void SetBooleanExTrueStrings(IEnumerable<string> trueStrings)
+        {
+            foreach (string trueString in trueStrings)
+            {
+                BooleanExTrueStrings.Add(trueString);
+            }
+        }
+
+        /// <summary>
+        /// Boolean类型添加False值字符串方法
+        /// </summary>
+        /// <param name="falseStrings">字符串集合</param>
+        public static void SetBooleanExFalseStrings(IEnumerable<string> falseStrings)
+        {
+            foreach (string falseString in falseStrings)
+            {
+                BooleanExFalseStrings.Add(falseString);
+            }
+        }
+
+        /// <summary>
+        /// BooleanEx类型转换方法
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static bool ToBooleanEx(string value)
+        {
+            var v = value?.ToLower()?.Trim() ?? "";
+            if (BooleanExTrueStrings.Contains(v)) return true;
+            if (BooleanExFalseStrings.Contains(v)) return false;
+            throw new ArgumentException("Unexpected Boolean Format");
+        }
+
         /// <summary>
         /// 通用转换方法
         /// </summary>
@@ -20,6 +70,8 @@ namespace CarrotBacktesting.Net.Common
                 return (T)(object)timeString.ParseDateTime();
             else if (dyn is DateTime time && default(T) is string)
                 return (T)(object)time.FormatDateTime();
+            else if (dyn is string boolString && default(T) is bool)
+                return (T)(object)ToBooleanEx(boolString);
             else
                 return Convert.ChangeType(dyn, typeof(T));
         }
@@ -44,6 +96,8 @@ namespace CarrotBacktesting.Net.Common
                     return (DateTime)(object)timeString.ParseDateTime();
                 else if (dyn is DateTime time && type == typeof(string))
                     return (string)(object)time.FormatDateTime();
+                else if (dyn is string boolString && type == typeof(bool))
+                    return (bool)(object)ToBooleanEx(boolString);
                 else
                     return Convert.ChangeType(dyn, type);
             }
