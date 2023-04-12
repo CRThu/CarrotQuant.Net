a7e738164af7b57306443959c105c61cdac8e064
Carrot <11172391+CRThu@users.noreply.github.com>
2023-04-13 01:06:46 +0800

    xml标签输出加锁与转义更新
    Former-commit-id: 91151bb6a2c5a4571338ac201de7edc4a4433dd2

--- CarrotQuant.Data.Python/print_xml.py
+++ CarrotQuant.Data.Python/print_xml.py
@@ -1,7 +1,19 @@
+import threading
 
+
+# 输出xml标签
+# 输出示例和输出结果
+# print_xml("Hello World!")             # <output>Hello World!</output>
+# print_xml("This is a test.", "test")  # <test>This is a test.</test>
+# print_xml("12345", "number")          # <number>12345</number>
 def print_xml(content, tag="output"):
     """
     This function takes in a string parameter and prints an xml tag with the content as its value.
     This function takes in a string parameter and prints an xml tag with the tag as its name.
     """
-    print("<" + tag + ">" + content + "</" + tag + ">")
+    lock = threading.Lock()
+    with lock:
+        content = content.replace("\n", "\\n")
+        content = content.replace("\r", "\\r")
+        content = content.replace("\t", "\\t")
+        print("<" + tag + ">" + content + "</" + tag + ">")
