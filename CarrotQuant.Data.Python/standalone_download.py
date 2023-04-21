0c22af5f16ea3d85d7b5f587d3daead3849cf562
Carrot <11172391+CRThu@users.noreply.github.com>
2023-04-22 01:26:25 +0800

    bugfix
    Former-commit-id: 395379bf7d0b0e66e33af53803999242f8a012a2

--- CarrotQuant.Data.Python/standalone_download.py
+++ CarrotQuant.Data.Python/standalone_download.py
@@ -7,7 +7,10 @@ if __name__ == '__main__':
     # os.environ["https_proxy"] = "http://127.0.0.1:7890"
 
     my_class = MyClass()
-    # my_class.download_ashare(start_time='1990-01-01', end_time='now', frequency='day', adjust='post')
-    # my_class.download_ashare(start_time='1990-01-01', end_time='now', frequency='day', adjust='none')
-    my_class.download_ashare(start_time='2010-01-01', end_time='now', frequency='5m', adjust='post')
-    # my_class.download_ashare(start_time='2010-01-01', end_time='now', frequency='5m', adjust='none')
+    start_time = '1990-01-01'
+    end_time = '2023-04-21'
+
+    my_class.download_ashare(start_time=start_time, end_time=end_time, frequency='day', adjust='post')
+    my_class.download_ashare(start_time=start_time, end_time=end_time, frequency='day', adjust='none')
+    # my_class.download_ashare(start_time=start_time, end_time=end_time, frequency='5m', adjust='post')
+    # my_class.download_ashare(start_time=start_time, end_time=end_time, frequency='5m', adjust='none')
