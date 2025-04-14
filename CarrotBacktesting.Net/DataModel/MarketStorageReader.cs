28fc515e7a5785f7d2d82ce6ed992e5a4d214255
Carrot <11172391+CRThu@users.noreply.github.com>
2025-04-14 23:07:08 +0800

    Create MarketStorageReader.cs

---
+++ CarrotBacktesting.Net/DataModel/MarketStorageReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.Net.DataModel
+{
+    public class MarketStorageReader
+    {
+        public MarketInternalStorageFrozenObject MarketStorage { get; init; }
+
+        public uint TickIndex { get; set; }
+
+
+        public MarketStorageReader(MarketInternalStorageFrozenObject storage)
+        {
+            MarketStorage = storage;
+        }
+
+        public 
+    }
+}
