370593eac271cb563011936fb28c8612bd8b414d
Carrot <11172391+CRThu@users.noreply.github.com>
2025-11-01 01:53:13 +0800

    增加空策略

---
+++ CarrotBacktesting.NET/Strategy/EmptyStrategy.cs
@@ -0,0 +1,22 @@
+﻿using CarrotBacktesting.NET.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotBacktesting.NET.Strategy
+{
+    /// <summary>
+    /// 空策略
+    /// </summary>
+    public class EmptyStrategy : ISignalStrategy
+    {
+        public string Name => nameof(EmptyStrategy);
+
+        public bool CheckSignal(SignalStrategyContext context)
+        {
+            return false;
+        }
+    }
+}
