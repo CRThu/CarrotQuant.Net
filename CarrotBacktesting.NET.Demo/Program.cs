6d81bddbb75dbc5ac9c515b510fad6dd7f609f63
Carrot <11172391+CRThu@users.noreply.github.com>
2026-05-16 01:09:51 +0800

    版本管理和build报错修复

--- CarrotBacktesting.NET.Demo/Program.cs
+++ CarrotBacktesting.NET.Demo/Program.cs
@@ -1,15 +1,4 @@
-﻿using CarrotBacktesting.NET.Config;
-using CarrotBacktesting.NET.Config.Model;
-using CarrotBacktesting.NET.Data;
-using CarrotBacktesting.NET.DataFeed;
-using CarrotBacktesting.NET.Engine;
-using CarrotBacktesting.NET.Result;
-using CarrotBacktesting.NET.Strategy;
-using CarrotBacktesting.NET.Examples;
-using CarrotBacktesting.NET.Utility;
-using CarrotBacktesting.NET.Utility.ScottPlot;
-using ScottPlot;
-using Spectre.Console;
+﻿
 
 namespace CarrotBacktesting.NET.Demo
 {
@@ -20,18 +9,9 @@ namespace CarrotBacktesting.NET.Demo
             Console.WriteLine("Hello, World!");
             try
             {
-                string configPath = ".\\CarrotQuant.Data\\v3\\yaml\\env.yaml";
-                BacktestingSession.Create(configPath)
-                    .LoadData()
-                    //.Run(new MarketTrendStrategy())
-                    //.Run(new MarketHeatStrategy())
-                    .Run(new VolumeSignalStrategy())
-                    //.Run(new PriceStrategy())
-                    .Analyze();
             }
             catch (Exception ex)
             {
-                // AnsiConsole.WriteException(ex, ExceptionFormats.ShortenPaths);
                 Console.WriteLine($"\nAn unhandled error occurred: {ex}");
             }
         }
